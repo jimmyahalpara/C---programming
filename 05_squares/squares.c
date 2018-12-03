a56b2104e7223ff564aacb9267c4a26c8275c150
Coursera Learner <nobody@nowhere.nul>
2018-12-03 13:42:55 +0000

    added recovery file

--- 05_squares/squares.c
+++ 05_squares/squares.c
@@ -11,7 +11,7 @@ int max(int x, int y){
 }
 
 int min(int x, int y){
-  if (x>y){
+  if (x<y){
     return x;
   }
   else {
@@ -23,9 +23,9 @@ void squares(int size1, int x_offset, int y_offset, int size2) {
   //compute the max of size1 and (x_offset + size2).  Call this w
   int w = max(size1, (x_offset + size2));
   //compute the max of size1 and (y_offset + size2).  Call this h
-  int h = max(size2, (y_offset + size2));
+  int h = max(size1, (y_offset + size2));
   //count from 0 to h. Call the number you count with y
-  for (int y = 0; y<h+1; y++) {
+  for (int y = 0; y<h; y++) {
 
     //count from 0 to w. Call the number you count with x
     for (int x = 0; x<w; x++){
@@ -36,22 +36,22 @@ void squares(int size1, int x_offset, int y_offset, int size2) {
       //    ((y is between y_offset and y_offset + size2) AND
       //     x is equal to either x_offset OR x_offset + size2 -1)
       // if so, print a *
-      if ((((x>=x_offset)&&(x< x_offset + size2)) && ((y == y_offset)||(y == y_offset + size2 - 1))) || (((y >= y_offset) && (y < y_offset + size2) ) && ((x == x_offset) || (x == x_offset + size2 -1)))){
+      if (((x>=x_offset && x<(x_offset + size2)) && (y==y_offset || y==(y_offset +size2 - 1))) || ((y>=y_offset && y<(y_offset + size2)) && (x==x_offset || x==(x_offset + size2 -1)))){
 	printf("*");
-	  }
+      } else {
       //if not,
-      else if (((x < size1) && (y == 0 || y == size1-1)) || ((y<size1) && ((x == 0) || (x == size1 - 1)))){
+      if ((x<size1 && (y==0 || y==(size1-1))) || (y<size1 && (x==0 || x==(size1-1)))){
       // check if EITHER
       //    x is less than size1 AND (y is either 0 or size1-1)
       // OR
       //    y is less than size1 AND (x is either 0 or size1-1)
       //if so, print a #
 	printf("#");
-	  }
+      }
       //else print a space
       else {
 	printf(" ");
-      }
+      }}
     }
     printf("\n");
       }
