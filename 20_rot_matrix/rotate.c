a49a84b6f06cd92e2c9840d32dc9922369ac8d5f
Coursera Learner <nobody@nowhere.nul>
2019-03-09 10:19:12 +0000

    done assignment 20

--- 20_rot_matrix/rotate.c
+++ 20_rot_matrix/rotate.c
@@ -4,9 +4,9 @@
 void rotate(char matrix[10][10]){
   for (int i = 0; i<10; i++){
     for (int j = 10-1; j>=0; j--){
-      print("%c", matrix[j][i]);
+      printf("%c", matrix[j][i]);
     }
-    print("\n");
+    printf("\n");
   }
   exit(0);
 }
