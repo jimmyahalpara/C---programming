8a8a2ba1aedb540cffbb3cdf067a3b7981157367
Coursera Learner <nobody@nowhere.nul>
2019-05-23 05:19:48 +0000

    done project

--- c2prj1_cards/cards.c
+++ c2prj1_cards/cards.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <assert.h>
 #include <stdlib.h>
@@ -5,12 +6,27 @@
 
 
 void assert_card_valid(card_t c) {
-  assert(c.value >= 2 && c.value <= 14);
-  assert(c.suit == SPADES || c.suit == CLUBS || c.suit == DIAMONDS || c.suit == HEARTS);
+  int cond1, cond2;
+  cond1 = (c.value >= 2) && (c.value <= VALUE_ACE);
+  cond2 = (c.suit == SPADES) || (c.suit == CLUBS) || (c.suit == HEARTS) || (c.suit == DIAMONDS);
+  assert(cond1 && cond2);
+  /* assert(c.value >= 2 && c.value <= 14);
+     assert(c.suit == SPADES || c.suit == CLUBS || c.suit == DIAMONDS || c.suit == HEARTS);*/
 }
 
 const char * ranking_to_string(hand_ranking_t r) {
-  if (r == STRAIGHT_FLUSH){
+  switch(r){
+  case STRAIGHT_FLUSH: return "STRAIGHT_FLUSH";
+  case FOUR_OF_A_KIND: return "FOUR_OF_A_KIND";
+  case FULL_HOUSE: return "FULL_HOUSE";
+  case FLUSH: return "FLUSH";
+  case STRAIGHT: return "STRAIGHT";
+  case THREE_OF_A_KIND: return "THREE_OF_A_KIND";
+  case TWO_PAIR: return "TWO_PAIR";
+  case PAIR: return "PAIR";
+  default : return "NOTHING";
+  }
+  /*if (r == STRAIGHT_FLUSH){
     return "STRAIGHT_FLUSH";
   }
   else if (r == FOUR_OF_A_KIND){
@@ -39,11 +55,23 @@ const char * ranking_to_string(hand_ranking_t r) {
   }
   else {
     return "INVALID";
-  }
+    }*/
 }
 
 char value_letter(card_t c) {
-  if (c.value >= 2 && c.value <= 9){
+  char x = '?';
+  if ((c.value >= 2) && (c.value <= 9)) x = '0' + c.value;
+  else {
+    switch(c.value){
+    case VALUE_KING : {x = 'K'; break;}
+    case 10: {x = '0'; break;}
+    case VALUE_ACE: {x = 'A'; break;}
+    case VALUE_QUEEN: {x = 'Q' ; break;}
+    case VALUE_JACK : {x = 'J' ; break;}
+    default: break;
+    }
+  }
+  /*if (c.value >= 2 && c.value <= 9){
     return '0' + c.value;
   }
   else if (c.value == 10){
@@ -63,12 +91,22 @@ char value_letter(card_t c) {
   }
   else {
     return '-';
-  }
+    }*/
+  return x;
 }
 
 
 char suit_letter(card_t c) {
-  if (c.suit == SPADES){
+  char x='?';
+  switch(c.suit){
+  case SPADES : {x = 's' ; break;}
+  case HEARTS : {x = 'h' ; break;}
+  case DIAMONDS : {x = 'd' ; break;}
+  case CLUBS : {x = 'c' ; break;}
+  default : {x = 'N' ; break;}
+  }
+  return x;
+  /*if (c.suit == SPADES){
     return 's';
   }
   else if (c.suit == HEARTS){
@@ -82,7 +120,7 @@ char suit_letter(card_t c) {
   }
   else {
     return '-';
-  }
+    }*/
   
 }
 
@@ -92,7 +130,24 @@ void print_card(card_t c) {
 
 card_t card_from_letters(char value_let, char suit_let) {
   card_t temp;
-  if (value_let == 'A'){
+  switch(value_let){
+  case 'K' : {temp.value = 13; break;}
+  case '0' : {temp.value = 10; break;}
+  case 'A' : {temp.value = 14; break;}
+  case 'Q' : {temp.value = 12; break;}
+  case 'J' : {temp.value = 11; break;}
+  default : {temp.value = value_let - '0'; break;}
+  }
+  switch(suit_let){
+  case 's' : {temp.suit = SPADES; break;}
+  case 'h' : {temp.suit = HEARTS; break;}
+  case 'd' : {temp.suit = DIAMONDS; break;}
+  case 'c' : {temp.suit = CLUBS; break;}
+  default: {temp.suit = NUM_SUITS; break;}
+  }
+  assert_card_valid(temp);
+  return temp;
+  /*if (value_let == 'A'){
     temp.value = 14;
   }
   else if (value_let == 'K'){
@@ -143,12 +198,24 @@ card_t card_from_letters(char value_let, char suit_let) {
   else if (suit_let == 'c'){
     temp.suit = CLUBS;
   }
-  return temp;
+  return temp;*/
 }
 
 card_t card_from_num(unsigned c) {
   card_t temp;
-  unsigned v = c%13+2;
+  unsigned value = c%13 + 2;
+  temp.value = value;
+
+  unsigned suit = c/13;
+  switch(suit){
+  case 0 : {temp.suit = SPADES; break;}
+  case 1 : {temp.suit = HEARTS; break;}
+  case 2 : {temp.suit = DIAMONDS; break;}
+  case 3 : {temp.suit = CLUBS; break;}
+  default : {temp.suit = NUM_SUITS; break;}
+  }
+  return temp;
+  /*unsigned v = c%13+2;
   card_t crd;
   crd.value = v;
   char value_l = value_letter(crd);
@@ -170,5 +237,5 @@ card_t card_from_num(unsigned c) {
     sl = ' ';
   }
   temp = card_from_letters(value_l, sl);
-  return temp;
+  return temp;*/
 }
