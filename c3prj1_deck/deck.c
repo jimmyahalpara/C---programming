70761b2c3666bed2f676e4b72e5d6e2c5097ade5
Coursera Learner <nobody@nowhere.nul>
2019-05-23 06:10:04 +0000

    done project

--- c3prj1_deck/deck.c
+++ c3prj1_deck/deck.c
@@ -9,29 +9,29 @@ int com(card_t c1, card_t c2){
 }
 void print_hand(deck_t * hand){
   card_t **card = hand -> cards;
-  card_t card1;
+  /*card_t card1;
   for (size_t i = 0; i < (hand -> n_cards); i++){
     card1  = ** (card + i);
     print_card(card1);
-  }
-  /*for (int i = 0; i < hand -> n_cards; i++){
+    }*/
+  for (int i = 0; i < hand -> n_cards; i++){
     print_card(**card);
     printf(" ");
     card++;
-    }*/
+    }
 }
 
 int deck_contains(deck_t * d, card_t c) {
   card_t **card = d -> cards;
-  for (size_t i = 0; i < d -> n_cards; i++){
+  /*for (size_t i = 0; i < d -> n_cards; i++){
     if (com(**(card + i), c)) return 1;
-  }
-  /*for (int i = 0; i < d -> n_cards; i++){
+    }*/
+  for (int i = 0; i < d -> n_cards; i++){
     if ((**card).suit == c.suit && (**card).value == c.value){
 	return 1;
       }
     card++;
-    }*/
+    }
   return 0;
 }
 
@@ -50,7 +50,7 @@ void shuffle(deck_t * d){
 }
 
 void assert_full_deck(deck_t * d) {
-  card_t ** card  = d -> cards;
+  /*card_t ** card  = d -> cards;
   card_t c;
   int count;
   for (size_t i = 0; i < d -> n_cards; i++){
@@ -60,15 +60,15 @@ void assert_full_deck(deck_t * d) {
       if (com(**(card+j), c)) count ++;
     }
     assert(count == 1);
-      }
-  /*card_t ** ptr_c = d -> cards;
+      }*/
+  card_t ** ptr_c = d -> cards;
   deck_t test_d;
   for (int i = 0; i < d -> n_cards - 1; i++){
     test_d.cards = d -> cards + i + 1;
     test_d.n_cards = d -> n_cards - i - 1;
     assert( deck_contains(&test_d, **ptr_c) == 0);
     ptr_c++;
-    }*/
+    }
 }
 
 void add_card_to(deck_t * deck, card_t c){
